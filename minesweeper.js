c81cacb758988d2228e53877a5d23e4c77378d7f
benjamin-means <ben.w.means@gmail.com>
2020-05-04 12:04:19 +1200

    add counting the boom minesweeper challenge

--- minesweeper.js
+++ minesweeper.js
@@ -6,25 +6,25 @@ var board = {
     {
       row: 0,
       col: 0,
-      isMine: true,
+      isMine: false,
       hidden: true,
     },
     {
       row: 0,
       col: 1,
-      isMine: true,
+      isMine: false,
       hidden: true,
     },
     {
       row: 0,
       col: 2,
-      isMine: true,
+      isMine: false,
       hidden: true,
     },
     {
       row: 1,
       col: 0,
-      isMine: true,
+      isMine: false,
       hidden: true,
     },
     {
@@ -54,13 +54,16 @@ var board = {
     {
       row: 2,
       col: 2,
-      isMine: true,
+      isMine: false,
       hidden: true,
     },
   ],
 };
 
 function startGame() {
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard();
 }
@@ -79,8 +82,17 @@ function checkForWin() {
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`:
 //
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+// var surrounding = lib.getSurroundingCells(cell.row, cell.col);
 //
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
-function countSurroundingMines(cell) {}
+function countSurroundingMines(cell) {
+  var count = 0;
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col);
+  for (var i = 0; i < surrounding.length; i++) {
+    if (cell.isMine == true) {
+      count++;
+    }
+    return count;
+  }
+}
